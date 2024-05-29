1b66cb99ce126a5a86b7a09b037628428e729920
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2024-05-29 11:50:08 -0500

    Moved file
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

--- pkg/scan/main_test.go
+++ pkg/scan/main_test.go
@@ -1,6 +1,4 @@
-//go:build e2e
-
-package main
+package scan
 
 import (
 	"context"
@@ -8,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/defenseunicorns/uds-security-hub/internal/log"
-	"github.com/defenseunicorns/uds-security-hub/pkg/scan"
 )
 
 func TestE2EScanFunctionality(t *testing.T) {
@@ -25,7 +22,7 @@ func TestE2EScanFunctionality(t *testing.T) {
 	tag := "9.8.0-uds.0-upstream"
 
 	// Create the scanner
-	scanner, err := scan.New(ctx, logger, trivyUsername, trivyPassword, ghcrToken)
+	scanner, err := New(ctx, logger, trivyUsername, trivyPassword, ghcrToken)
 	if err != nil {
 		t.Fatalf("Error creating scanner: %v", err)
 	}
