0da5e92696b47550f26ba7514290fd23f0fd6507
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2024-07-30 16:51:33 -0500

    Removed the unused test
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

--- internal/sql/e2e_test.go
+++ internal/sql/e2e_test.go
@@ -1,7 +1,6 @@
 package sql
 
 import (
-	"context"
 	"fmt"
 	"testing"
 )
@@ -48,58 +47,6 @@ func TestCreateDBConnector(t *testing.T) {
 			connector := CreateDBConnector(tt.host, tt.port, tt.user, tt.password, tt.dbname, tt.instanceConnectionName)
 			if gotType := fmt.Sprintf("%T", connector); gotType != tt.expectedType {
 				t.Errorf("CreateDBConnector() = %v, want %v", gotType, tt.expectedType)
-			}
-		})
-	}
-}
-func TestConnect(t *testing.T) {
-	tests := []struct {
-		name      string
-		connector *StandardDBConnector
-		expectErr bool
-	}{
-		{
-			name: "Success",
-			connector: &StandardDBConnector{
-				host:     "localhost",
-				port:     "5432",
-				user:     "test_user",
-				password: "test_password",
-				dbname:   "test_db",
-			},
-			expectErr: false,
-		},
-		{
-			name: "Failure",
-			connector: &StandardDBConnector{
-				host:     "invalid_host",
-				port:     "5432",
-				user:     "test_user",
-				password: "test_password",
-				dbname:   "test_db",
-			},
-			expectErr: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			db, err := tt.connector.Connect(context.Background())
-			if (err != nil) != tt.expectErr {
-				t.Fatalf("Connect() error = %v, expectErr %v", err, tt.expectErr)
-			}
-			if tt.expectErr {
-				return
-			}
-
-			sqlDB, err := db.DB()
-			if err != nil {
-				t.Fatalf("db.DB() error = %v", err)
-			}
-			defer sqlDB.Close()
-
-			if err := sqlDB.Ping(); err != nil {
-				t.Fatalf("sqlDB.Ping() error = %v", err)
 			}
 		})
 	}
