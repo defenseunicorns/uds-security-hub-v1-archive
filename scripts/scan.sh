bf06de0858563f23b91ccf797288f23844933a8a
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2024-08-29 11:49:56 -0500

    Cleaned up registry creds
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -12,17 +12,14 @@
 #
 # Environment Variables:
 #   GHCR_CREDS                : Credentials for GitHub Container Registry.
-#   REGISTRY1_CREDS           : Credentials for the first registry.
 #   GITHUB_TOKEN              : GitHub token for authentication.
-#   DOCKER_IO_CREDS           : Credentials for Docker.io.
 #   DB_NAME                   : Database name.
 #   DB_USER                   : Database user.
 #   DB_PASSWORD               : Database password.
 #   INSTANCE_CONNECTION_NAME  : Instance connection name.
-#   CGR_CREDS                 : Credentials for the Container Registry.
 
 # Check if necessary environment variables are set
-required_vars=(GHCR_CREDS REGISTRY1_CREDS GITHUB_TOKEN DOCKER_IO_CREDS DB_NAME DB_USER DB_PASSWORD INSTANCE_CONNECTION_NAME CGR_CREDS)
+required_vars=(GHCR_CREDS GITHUB_TOKEN DB_NAME DB_USER DB_PASSWORD INSTANCE_CONNECTION_NAME)
 for var in "${required_vars[@]}"; do
   if [ -z "${!var}" ]; then
     echo "Environment variable $var is not set. Please set it before running the script."
@@ -72,9 +69,6 @@ while IFS= read -r NAME; do
     -v "${NUMBER_OF_VERSIONS}" \
     -t "${GITHUB_TOKEN}" \
     --registry-creds "${GHCR_CREDS}" \
-    --registry-creds "${REGISTRY1_CREDS}" \
-    --registry-creds "${DOCKER_IO_CREDS}" \
-    --registry-creds "${CGR_CREDS}" \
     --instance-connection-name "${INSTANCE_CONNECTION_NAME}" \
     --db-name "${DB_NAME}" \
     --db-user "${DB_USER}" \
