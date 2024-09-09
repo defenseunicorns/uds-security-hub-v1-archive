47e9d89c3971ceffca6b22ec6d9586b568f58e06
partkyle <partkyle@defenseunicorns.com>
2024-09-09 11:18:55 -0700

    table driven result set

--- pkg/scan/scan_result_reader_test.go
+++ pkg/scan/scan_result_reader_test.go
@@ -8,52 +8,152 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/defenseunicorns/uds-security-hub/pkg/types"
 )
 
 func TestWriteToJSON(t *testing.T) {
-	scanResult := types.ScanResult{
-		ArtifactName: "test-artifact",
-		Results: []struct {
-			Vulnerabilities []types.VulnerabilityInfo `json:"Vulnerabilities"`
-		}{
-			{
-				Vulnerabilities: []types.VulnerabilityInfo{
-					{
-						VulnerabilityID:  "CVE-2021-1234",
-						PkgName:          "test-package",
-						InstalledVersion: "1.0.0",
-						FixedVersion:     "1.0.1",
-						Severity:         "HIGH",
-						Description:      "Test vulnerability",
+	tests := []struct {
+		name     string
+		input    []types.ScanResultReader
+		expected []JSONOutputEntry
+	}{
+		{
+			name: "single result",
+			input: []types.ScanResultReader{
+				types.ScanResult{
+					ArtifactName: "test-artifact",
+					Results: []struct {
+						Vulnerabilities []types.VulnerabilityInfo `json:"Vulnerabilities"`
+					}{
+						{
+							Vulnerabilities: []types.VulnerabilityInfo{
+								{
+									VulnerabilityID:  "CVE-2021-1234",
+									PkgName:          "test-package",
+									InstalledVersion: "1.0.0",
+									FixedVersion:     "1.0.1",
+									Severity:         "HIGH",
+									Description:      "Test vulnerability",
+								},
+							},
+						},
 					},
+				},
+			},
+			expected: []JSONOutputEntry{
+				{
+					ArtifactName:     "test-artifact",
+					VulnerabilityID:  "CVE-2021-1234",
+					PkgName:          "test-package",
+					InstalledVersion: "1.0.0",
+					FixedVersion:     "1.0.1",
+					Severity:         "HIGH",
+					Description:      "Test vulnerability",
+				},
+			},
+		},
+		{
+			name: "multiple result sets",
+			input: []types.ScanResultReader{
+				types.ScanResult{
+					ArtifactName: "test-artifact",
+					Results: []struct {
+						Vulnerabilities []types.VulnerabilityInfo `json:"Vulnerabilities"`
+					}{
+						{
+							Vulnerabilities: []types.VulnerabilityInfo{
+								{
+									VulnerabilityID:  "CVE-2021-1234",
+									PkgName:          "test-package-1-1",
+									InstalledVersion: "1.0.0",
+									FixedVersion:     "1.0.1",
+									Severity:         "HIGH",
+									Description:      "Test vulnerability",
+								},
+							},
+						},
+					},
+				},
+				types.ScanResult{
+					ArtifactName: "test-artifact-2",
+					Results: []struct {
+						Vulnerabilities []types.VulnerabilityInfo `json:"Vulnerabilities"`
+					}{
+						{
+							Vulnerabilities: []types.VulnerabilityInfo{
+								{
+									VulnerabilityID:  "CVE-2021-2345",
+									PkgName:          "test-package-2-1",
+									InstalledVersion: "1.0.0",
+									FixedVersion:     "3.0.0",
+									Severity:         "HIGH",
+									Description:      "Test vulnerability",
+								},
+								{
+									VulnerabilityID:  "CVE-2021-3456",
+									PkgName:          "test-package-2-2",
+									InstalledVersion: "2.0.0",
+									FixedVersion:     "3.0.0",
+									Severity:         "HIGH",
+									Description:      "Test vulnerability",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: []JSONOutputEntry{
+				{
+					ArtifactName:     "test-artifact",
+					VulnerabilityID:  "CVE-2021-1234",
+					PkgName:          "test-package-1-1",
+					InstalledVersion: "1.0.0",
+					FixedVersion:     "1.0.1",
+					Severity:         "HIGH",
+					Description:      "Test vulnerability",
+				},
+				{
+					ArtifactName:     "test-artifact-2",
+					VulnerabilityID:  "CVE-2021-2345",
+					PkgName:          "test-package-2-1",
+					InstalledVersion: "1.0.0",
+					FixedVersion:     "3.0.0",
+					Severity:         "HIGH",
+					Description:      "Test vulnerability",
+				},
+				{
+					ArtifactName:     "test-artifact-2",
+					VulnerabilityID:  "CVE-2021-3456",
+					PkgName:          "test-package-2-2",
+					InstalledVersion: "2.0.0",
+					FixedVersion:     "3.0.0",
+					Severity:         "HIGH",
+					Description:      "Test vulnerability",
 				},
 			},
 		},
 	}
 
-	var buf bytes.Buffer
-	err := WriteToJSON(&buf, []types.ScanResultReader{scanResult}) // Call method on reader
-	require.NoError(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			err := WriteToJSON(&buf, tt.input) // Call method on reader
+			if err != nil {
+				t.Fatalf("failed to WriteToJSON: %v", err)
+			}
 
-	var output []JSONOutputEntry
-	err = json.Unmarshal(buf.Bytes(), &output)
-	require.NoError(t, err)
+			var output []JSONOutputEntry
+			err = json.Unmarshal(buf.Bytes(), &output)
+			if err != nil {
+				t.Fatalf("failed to Unmarshal output: %v", err)
+			}
 
-	assert.Equal(t, len(output), 1, "there should only be 1 result in this output")
-
-	firstResult := output[0]
-
-	require.Equal(t, "test-artifact", firstResult.ArtifactName)
-	assert.Equal(t, "CVE-2021-1234", firstResult.VulnerabilityID)
-	assert.Equal(t, "test-package", firstResult.PkgName)
-	assert.Equal(t, "1.0.0", firstResult.InstalledVersion)
-	assert.Equal(t, "1.0.1", firstResult.FixedVersion)
-	assert.Equal(t, "HIGH", firstResult.Severity)
-	assert.Equal(t, "Test vulnerability", firstResult.Description)
+			if diff := cmp.Diff(output, tt.expected); diff != "" {
+				t.Errorf("WriteToJSON() mismatch (-got +want):\n%s", diff)
+			}
+		})
+	}
 }
 
 func TestWriteToCSV(t *testing.T) {
@@ -161,7 +261,8 @@ func TestWriteToCSV(t *testing.T) {
 				{
 					ArtifactName: "override-artifact-name:v0.1.1",
 					Results: []struct {
-						Vulnerabilities []types.VulnerabilityInfo `json:"Vulnerabilities"`
+						Vulnerabilities []types.
+							VulnerabilityInfo `json:"Vulnerabilities"`
 					}{
 						{
 							Vulnerabilities: []types.VulnerabilityInfo{
