879d3046d5f6ad36c51920858d71a7854d9f99de
Ben Wilcox <benjamin@defenseunicorns.com>
2024-09-09 14:05:31 -0400

    added tests for semver.go

--- pkg/semver/semver_test.go
+++ pkg/semver/semver_test.go
@@ -46,3 +46,48 @@ func TestGetNMinusCustomExcludeSemvers(t *testing.T) {
 		}
 	}
 }
+
+func TestGetNMinusTwoSemversInvalidN(t *testing.T) {
+	versions := []string{"1.0.0", "1.1.0", "1.2.0", "2.0.0", "2.1.0"}
+	n := 1 // n is less than excludeCount (which defaults to 2)
+
+	_, err := GetNMinusTwoSemvers(versions, n)
+	if err == nil {
+		t.Fatal("Expected an error but got none")
+	}
+
+	expectedError := "n must be at least 2"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error message '%s', got '%s'", expectedError, err.Error())
+	}
+}
+
+func TestGetNMinusTwoSemversNotEnoughVersions(t *testing.T) {
+	versions := []string{"1.0.0"}
+	n := 3 // More versions requested than available
+
+	_, err := GetNMinusTwoSemvers(versions, n)
+	if err == nil {
+		t.Fatal("Expected an error but got none")
+	}
+
+	expectedError := "not enough versions to get n-2"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error message '%s', got '%s'", expectedError, err.Error())
+	}
+}
+
+func TestGetNMinusTwoSemversInvalidVersion(t *testing.T) {
+	versions := []string{"1.0.0", "invalid_version", "2.0.0"} // invalid_version is not a valid semver
+	n := 3
+
+	_, err := GetNMinusTwoSemvers(versions, n)
+	if err == nil {
+		t.Fatal("Expected an error but got none")
+	}
+
+	expectedError := "invalid semver: invalid_version"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error message '%s', got '%s'", expectedError, err.Error())
+	}
+}
