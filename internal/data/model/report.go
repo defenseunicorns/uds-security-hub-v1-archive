86af0263f01a1b5e031a396c760e855108c0ddbb
Kyle Partridge <partkyle@defenseunicorns.com>
2024-09-25 14:14:48 -0700

    fix: update column to reflect type (#294)

--- internal/data/model/report.go
+++ internal/data/model/report.go
@@ -10,7 +10,7 @@ type Report struct {
 	CreatedAt   time.Time       `json:"CreatedAt" gorm:"autoCreateTime"`
 	PackageName string          `json:"PackageName" gorm:"not null" index:"idx_package_name"`
 	Tag         string          `json:"Tag" gorm:"not null" index:"idx_tag"`
-	SBOM        json.RawMessage `json:"SBOM"`
+	SBOM        json.RawMessage `json:"SBOM" gorm:"type:jsonb"`
 	ID          uint            `json:"ID" gorm:"primaryKey;autoIncrement"`
 	Critical    int             `json:"Critical"`
 	High        int             `json:"High"`
